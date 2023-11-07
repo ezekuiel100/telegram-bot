64f5d7e0557aa6be71a71f4bc8710485e859e431
unknown <ezekuiel100@gmail.com>
2023-11-07 11:24:57 -0300

    Resolving bugs

--- main.js
+++ main.js
@@ -2,14 +2,13 @@ const TelegramBot = require("node-telegram-bot-api");
 require("dotenv").config();
 
 const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
-
-// const TOKEN = process.env.TOKEN;
+// const telegramBotToken = process.env.token;
 
 const bot = new TelegramBot(telegramBotToken, { polling: true });
 
 bot.on("message", (msg) => {
-  if (msg.entities) {
-    bot.sendMessage(msg.chat.id, "PROIBIDO LINK NO GRUPO!");
+  if (msg.entities[0].type == "url") {
+    bot.sendMessage(msg.chat.id, "PROIBIDO LINKS NO GRUPO!");
     bot.deleteMessage(msg.chat.id, msg.message_id);
   }
 });
