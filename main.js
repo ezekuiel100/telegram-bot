5837965dcbaa97862010d2c0b6f42ad143ebba9f
unknown <ezekuiel100@gmail.com>
2023-11-09 13:08:48 -0300

    Restricting member

--- main.js
+++ main.js
@@ -1,43 +1,43 @@
 const TelegramBot = require("node-telegram-bot-api");
 require("dotenv").config();
 
-const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
-//const telegramBotToken = process.env.token;
+//const telegramBotToken = process.env.TELEGRAM_BOT_TOKEN;
+const telegramBotToken = process.env.token;
 
 const bot = new TelegramBot(telegramBotToken, { polling: true });
 
 let Administradores = [];
 
-bot.on("message", async (msg) => {
-  try {
-    if (msg?.entities && msg.entities[0].type == "url") {
-      await GetGroupAdmins(msg);
-
-      if (Administradores.includes(msg.from.id)) return;
+bot.on("message", (msg) => {
+  if (msg?.entities && msg.entities[0].type == "url") {
+    GetGroupAdmins(msg).then(() => {
       DeleteGroupMessage(msg);
-    }
+    });
+  }
 
-    if ((msg.photo || msg.video) && msg.caption_entities[0].type == "url") {
+  if ((msg.photo || msg.video) && msg.caption_entities[0].type == "url") {
+    GetGroupAdmins(msg).then(() => {
       DeleteGroupMessage(msg);
-    }
-  } catch (error) {
-    console.log("Erro: " + error.message);
+    });
   }
 });
 
-//restringir quem manda links
-//impedir encaminhamentos
-//img com links esta apagando msg do admin
-
 function DeleteGroupMessage(msg) {
+  if (Administradores.includes(msg.from.id)) return;
   bot.sendMessage(msg.chat.id, "PROIBIDO LINKS NO GRUPO!");
   bot.deleteMessage(msg.chat.id, msg.message_id);
+  restrictChatMember(msg);
 }
 
-function GetGroupAdmins(msg) {
-  bot.getChatAdministrators(msg.chat.id).then((admin) => {
-    admin.map((adm) => {
-      Administradores.push(adm.user.id);
-    });
+async function GetGroupAdmins(msg) {
+  let admin = await bot.getChatAdministrators(msg.chat.id);
+  admin.map((adm) => Administradores.push(adm.user.id));
+}
+
+function restrictChatMember(msg) {
+  let data = Date.now();
+  console.log(data);
+  bot.restrictChatMember(msg.chat.id, msg.from.id, {
+    can_send_messages: false,
   });
 }
